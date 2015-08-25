mozilla/fxa-content-server

fix(settings): fix gravatar permission screen within modal

app/scripts/views/settings/avatar_change.js
@@ -41,8 +41,14 @@ function ($, Cocktail, FormView, AvatarMixin, SettingsMixin, SettingsPanelMixin,
     },
 
     afterVisible: function () {
-      FormView.prototype.afterVisible.call(this);
-      return this.displayAccountProfileImage(this.getSignedInAccount());
+      var self = this;
+      FormView.prototype.afterVisible.call(self);
+      return self.displayAccountProfileImage(self.getSignedInAccount())
+        .then(function () {
+          if (self.getSignedInAccount().has('profileImageUrl')) {
+            self.$('.remove').css('display', 'inline-block');
+          }
+        });
     },
 
     afterRender: function () {

dannycoates/fxa-local-dev

fxa-content-server/app/scripts/views/settings/avatar_change.js
@@ -48,8 +48,14 @@ function ($, Cocktail, FormView, AvatarMixin, SettingsMixin, SettingsPanelMixin,
     },
 
     afterVisible: function () {
-      FormView.prototype.afterVisible.call(this);
-      return this.displayAccountProfileImage(this.getAccount());
+      var self = this;
+      FormView.prototype.afterVisible.call(self);
+      return self.displayAccountProfileImage(self.getAccount())
+        .then(function () {
+          if (self.getAccount().has('profileImageUrl')) {
+            self.$('.remove').css('display', 'inline-block');
+          }
+        });
     },
 
     afterRender: function () {