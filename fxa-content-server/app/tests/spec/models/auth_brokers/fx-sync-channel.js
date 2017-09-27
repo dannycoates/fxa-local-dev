mozilla/fxa-content-server

commit d31de3522a6e57bf172009ad5eb3a9369eff3624

feat(sync): Transition screens after login message in Fx >= 57
fixes #5197

app/scripts/models/auth_brokers/fx-sync-channel.js
@@ -268,14 +268,22 @@ define(function (require, exports, module) {
       return ! _.difference(REQUIRED_LOGIN_FIELDS, loginFields).length;
     },
 
+    /**
+     * Get login data from `account` to send to the browser.
+     * All returned keys have a defined value.
+     *
+     * @param {Object} account
+     * @returns {Object}
+     * @private
+     */
     _getLoginData (account) {
       const loginData = account.pick(ALLOWED_LOGIN_FIELDS);
       loginData.verified = !! loginData.verified;
       loginData.verifiedCanLinkAccount = !! this._verifiedCanLinkEmail &&
                                          !! this._verifiedCanLinkUid;
-      return loginData;
-    }
 
+      return _.omit(loginData, _.isUndefined);
+    }
   }, {
     REQUIRED_LOGIN_FIELDS
   });

app/tests/spec/models/auth_brokers/fx-sync-channel.js
@@ -22,9 +22,7 @@ define(function (require, exports, module) {
     let user;
     let windowMock;
 
-    function createAuthBroker (options) {
-      options = options || {};
-
+    function createAuthBroker (options = {}) {
       broker = new FxSyncChannelAuthenticationBroker(_.extend({
         channel: channelMock,
         commands: {
@@ -156,8 +154,16 @@ define(function (require, exports, module) {
       const requiredAccountFields = _.without(FxSyncChannelAuthenticationBroker.REQUIRED_LOGIN_FIELDS, 'verified');
 
       requiredAccountFields.forEach(function (fieldName) {
-        it('does not send a `login` message to the channel if the account does not have `' + fieldName + '`', () => {
+        it(`does not send a \`login\` message if the account does not have \`${fieldName}\``, () => {
           account.unset(fieldName);
+          return broker._notifyRelierOfLogin(account)
+            .then(() => {
+              assert.isFalse(channelMock.send.called);
+            });
+        });
+
+        it(`does not send a \`login\` message if \`${fieldName}\` is undefined`, () => {
+          account.set(fieldName, undefined);
           return broker._notifyRelierOfLogin(account)
             .then(() => {
               assert.isFalse(channelMock.send.called);

app/scripts/lib/user-agent-mixin.js
@@ -6,15 +6,19 @@
  * Get the user agent string, or a user agent parser.
  *
  * Requires `this.window` to be set.
- * Requires the `SearchParamMixin`.
  */
 
 define((require, exports, module) => {
   'use strict';
 
+  const SearchParamMixin = require('lib/search-param-mixin');
   const UserAgent = require('lib/user-agent');
 
   module.exports = {
+    dependsOn: [
+      SearchParamMixin
+    ],
+
     /**
      * Get the user-agent string. For functional testing
      * purposes, first attempts to fetch a UA string from the

app/scripts/models/auth_brokers/base.js
@@ -426,6 +426,11 @@ define(function (require, exports, module) {
        */
       allowUidChange: false,
       /**
+       * Does the browser handle screen transitions after
+       * an email verification?
+       */
+      browserTransitionsAfterEmailVerification: true,
+      /**
        * Should the signup page show the `Choose what to sync` checkbox
        */
       chooseWhatToSyncCheckbox: true,

app/scripts/models/auth_brokers/fx-desktop-v2.js
@@ -15,45 +15,27 @@ define(function (require, exports, module) {
 
   const _ = require('underscore');
   const FxSyncWebChannelAuthenticationBroker = require('./fx-sync-web-channel');
-  const HaltBehavior = require('views/behaviors/halt');
-  const NullBehavior = require('views/behaviors/null');
-  const p = require('lib/promise');
+  const HaltIfBrowserTransitions = require('views/behaviors/halt-if-browser-transitions');
 
-  var proto = FxSyncWebChannelAuthenticationBroker.prototype;
+  const proto = FxSyncWebChannelAuthenticationBroker.prototype;
+  const defaultBehaviors = proto.defaultBehaviors;
 
-  var FxDesktopV2AuthenticationBroker = FxSyncWebChannelAuthenticationBroker.extend({
-    defaultBehaviors: _.extend({}, proto.defaultBehaviors, {
-      // about:accounts displays its own screen after sign in, no need
-      // to show anything.
-      afterForceAuth: new HaltBehavior(),
-      // about:accounts displays its own screen after password reset, no
-      // need to show anything.
-      afterResetPasswordConfirmationPoll: new HaltBehavior(),
-      // about:accounts displays its own screen after sign in, no need
-      // to show anything.
-      afterSignIn: new HaltBehavior(),
-      // the browser is already polling, no need for the content server
-      // code to poll as well, otherwise two sets of polls are going on
-      // for the same user.
-      beforeSignUpConfirmationPoll: new HaltBehavior()
+  const FxDesktopV2AuthenticationBroker = FxSyncWebChannelAuthenticationBroker.extend({
+    defaultBehaviors: _.extend({}, defaultBehaviors, {
+      afterForceAuth: new HaltIfBrowserTransitions(defaultBehaviors.afterForceAuth),
+      afterResetPasswordConfirmationPoll: new HaltIfBrowserTransitions(defaultBehaviors.afterResetPasswordConfirmationPoll),
+      afterSignIn: new HaltIfBrowserTransitions(defaultBehaviors.afterSignIn),
+      beforeSignUpConfirmationPoll: new HaltIfBrowserTransitions(defaultBehaviors.beforeSignUpConfirmationPoll),
     }),
 
     defaultCapabilities: _.extend({}, proto.defaultCapabilities, {
+      browserTransitionsAfterEmailVerification: true,
       chooseWhatToSyncCheckbox: false,
       chooseWhatToSyncWebV1: true,
       openWebmailButtonVisible: true
     }),
 
     type: 'fx-desktop-v2',
-
-    afterResetPasswordConfirmationPoll (/*account*/) {
-      // this is only called if the user verifies in the same browser.
-      // With Fx's E10s enabled, the account data only contains an
-      // unwrapBKey and keyFetchToken, not enough to sign in the user.
-      // Luckily, with WebChannels, the verification page can send
-      // the data to the browser and everybody is happy
-      return p(new HaltBehavior());
-    },
 
     afterCompleteResetPassword (account) {
       // See the note in afterResetPasswordConfirmationPoll
@@ -64,15 +46,7 @@ define(function (require, exports, module) {
     fetch () {
       return proto.fetch.call(this).then(() => {
         if (! this.environment.isAboutAccounts()) {
-          // The default behavior of FxDesktop brokers is to halt before
-          // the signup confirmation poll because about:accounts takes care
-          // of polling and updating the UI. However if we are not in about:accounts
-          // we do not want the halting behavior.
-          this._behaviors.keys().forEach((behaviorName) => {
-            if (this.getBehavior(behaviorName).type === 'halt') {
-              this.setBehavior(behaviorName, new NullBehavior());
-            }
-          });
+          this.setCapability('browserTransitionsAfterEmailVerification', false);
         }
       });
     }

app/scripts/models/auth_brokers/fx-desktop-v3.js
@@ -13,18 +13,34 @@ define(function (require, exports, module) {
   'use strict';
 
   const _ = require('underscore');
+  const Cocktail = require('cocktail');
   const FxDesktopV2AuthenticationBroker = require('./fx-desktop-v2');
+  const UserAgentMixin = require('lib/user-agent-mixin');
 
-  var proto = FxDesktopV2AuthenticationBroker.prototype;
+  const proto = FxDesktopV2AuthenticationBroker.prototype;
 
-  var FxDesktopV3AuthenticationBroker = FxDesktopV2AuthenticationBroker.extend({
+  const FxDesktopV3AuthenticationBroker = FxDesktopV2AuthenticationBroker.extend({
     defaultCapabilities: _.extend({}, proto.defaultCapabilities, {
       allowUidChange: true,
       emailFirst: true
     }),
 
-    type: 'fx-desktop-v3'
+    type: 'fx-desktop-v3',
+
+    fetch () {
+      return proto.fetch.call(this).then(() => {
+        const userAgent = this.getUserAgent();
+        if (userAgent.parseVersion().major >= 57) {
+          this.setCapability('browserTransitionsAfterEmailVerification', false);
+        }
+      });
+    }
   });
+
+  Cocktail.mixin(
+    FxDesktopV3AuthenticationBroker,
+    UserAgentMixin
+  );
 
   module.exports = FxDesktopV3AuthenticationBroker;
 });

app/scripts/views/behaviors/halt-if-browser-transitions.js
@@ -1,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * A behavior that halts if the browser transitions after
+ * it detects an email verification. If browser does not
+ * transition, `defaultBehavior` is returned instead.
+ */
+
+define(function (require, exports, module) {
+  'use strict';
+
+  const HaltBehavior = require('views/behaviors/halt');
+
+  module.exports = function (defaultBehavior) {
+    const behavior = function (view) {
+      if (view.broker.getCapability('browserTransitionsAfterEmailVerification')) {
+        return new HaltBehavior();
+      }
+
+      return defaultBehavior;
+    };
+
+    behavior.type = 'halt-if-browser-transitions';
+
+    return behavior;
+  };
+});

app/tests/spec/models/auth_brokers/fx-desktop-v1.js
@@ -40,6 +40,7 @@ define(function (require, exports, module) {
         channel: channelMock,
         window: windowMock
       });
+      sinon.stub(broker, '_hasRequiredLoginFields').callsFake(() => true);
     });
 
     it('has the `signup` capability by default', function () {

dannycoates/fxa-local-dev

commit 906b5ccdbc4d9c0b74bb53bf095f18f6d679147a

fix(test): Fix the Sync v1 reset_password test. (#5529) r=@vbudhram, @vladikoff
The `customizeSync` attribute was not available on the account.

This is a regression due to us filtering out `undefined` values
on login data, introduced in d31de3522a6e57bf172009ad5eb3a9369eff3624.

fixes #5528

fxa-content-server/app/scripts/models/auth_brokers/fx-sync-channel.js
@@ -182,6 +182,15 @@ define(function (require, exports, module) {
     },
 
     afterResetPasswordConfirmationPoll (account) {
+      // We wouldn't expect `customizeSync` to be set when completing
+      // a password reset, but the field must be present for the login
+      // message to be sent. false is the default value set in
+      // lib/fxa-client.js if the value is not present.
+      // See #5528
+      if (! account.has('customizeSync')) {
+        account.set('customizeSync', false);
+      }
+
       return this._notifyRelierOfLogin(account)
         .then(() => proto.afterResetPasswordConfirmationPoll.call(this, account));
     },

fxa-content-server/app/tests/spec/models/auth_brokers/fx-sync-channel.js
@@ -368,9 +368,19 @@ define(function (require, exports, module) {
 
     describe('afterResetPasswordConfirmationPoll', () => {
       it('notifies the channel of login, does not halt by default', () => {
+        // customizeSync is required to send the `login` message, but
+        // it won't be set because the user hasn't visited the signup/in
+        // page.
+
+        account.unset('customizeSync');
+
         return broker.afterResetPasswordConfirmationPoll(account)
           .then(function (result) {
+            assert.isTrue(channelMock.send.calledOnce);
             assert.isTrue(channelMock.send.calledWith('login'));
+            const loginData = channelMock.send.args[0][1];
+            assert.isFalse(loginData.customizeSync);
+
             assert.isUndefined(result.halt);
           });
       });