mozilla/fxa-auth-server

chore(logging): log email headers to diagnose #2133

test/local/email/utils.js
@@ -183,6 +183,7 @@ describe('email utils helpers', () => {
         op: 'emailHeaders.missing',
         origin: 'wibble'
       }, log.error.args[0][0])
+      assert.equal(log.info.callCount, 0)
     })
 
     it('logs an error if message.mail.headers is missing', () => {
@@ -192,16 +193,45 @@ describe('email utils helpers', () => {
         op: 'emailHeaders.missing',
         origin: 'blee'
       }, log.error.args[0][0])
+      assert.equal(log.info.callCount, 0)
     })
 
     it('does not log an error if message.mail.headers is object', () => {
-      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, { mail: { headers: {} } })
+      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, {
+        mail: {
+          headers: {
+            'X-Template-Name': 'foo',
+            'X-Xxx': 'bar',
+            'X-Yyy': 'baz',
+            'X-Zzz': 'qux'
+          }
+        }
+      }, 'wibble')
       assert.equal(log.error.callCount, 0)
+      assert.equal(log.info.callCount, 1)
+      assert.equal(log.info.args[0].length, 1)
+      assert.deepEqual(log.info.args[0][0], {
+        op: 'emailHeaders.keys',
+        keys: [ 'X-Template-Name', 'X-Xxx', 'X-Yyy', 'X-Zzz' ],
+        template: 'foo',
+        origin: 'wibble'
+      })
     })
 
     it('does not log an error if message.headers is object', () => {
-      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, { headers: {} })
+      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, {
+        headers: {
+          'x-template-name': 'wibble'
+        }
+      }, 'blee')
       assert.equal(log.error.callCount, 0)
+      assert.equal(log.info.callCount, 1)
+      assert.deepEqual(log.info.args[0][0], {
+        op: 'emailHeaders.keys',
+        keys: [ 'x-template-name' ],
+        template: 'wibble',
+        origin: 'blee'
+      })
     })
 
     it('logs an error if message.mail.headers is non-object', () => {
@@ -212,6 +242,7 @@ describe('email utils helpers', () => {
         type: 'string',
         origin: 'wibble'
       }, log.error.args[0][0])
+      assert.equal(log.info.callCount, 0)
     })
 
     it('logs an error if message.headers is non-object', () => {
@@ -222,6 +253,7 @@ describe('email utils helpers', () => {
         type: 'number',
         origin: 'wibble'
       }, log.error.args[0][0])
+      assert.equal(log.info.callCount, 0)
     })
   })
 })

dannycoates/fxa-local-dev

fxa-auth-server/test/local/email/utils.js
@@ -183,6 +183,7 @@ describe('email utils helpers', () => {
         op: 'emailHeaders.missing',
         origin: 'wibble'
       }, log.error.args[0][0])
+      assert.equal(log.warn.callCount, 0)
     })
 
     it('logs an error if message.mail.headers is missing', () => {
@@ -192,16 +193,93 @@ describe('email utils helpers', () => {
         op: 'emailHeaders.missing',
         origin: 'blee'
       }, log.error.args[0][0])
+      assert.equal(log.warn.callCount, 0)
     })
 
-    it('does not log an error if message.mail.headers is object', () => {
-      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, { mail: { headers: {} } })
+    it('does not log an error/warning if message.mail.headers is object and deviceId is set', () => {
+      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, {
+        mail: {
+          headers: {
+            'X-Device-Id': 'foo'
+          }
+        }
+      })
       assert.equal(log.error.callCount, 0)
+      assert.equal(log.warn.callCount, 0)
     })
 
-    it('does not log an error if message.headers is object', () => {
-      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, { headers: {} })
+    it('does not log an error/warning if message.mail.headers is object and deviceId is set (lowercase)', () => {
+      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, {
+        mail: {
+          headers: {
+            'x-device-id': 'bar'
+          }
+        }
+      })
       assert.equal(log.error.callCount, 0)
+      assert.equal(log.warn.callCount, 0)
+    })
+
+    it('does not log an error/warning if message.mail.headers is object and uid is set', () => {
+      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, {
+        mail: {
+          headers: {
+            'X-Uid': 'foo'
+          }
+        }
+      })
+      assert.equal(log.error.callCount, 0)
+      assert.equal(log.warn.callCount, 0)
+    })
+
+    it('does not log an error/warning if message.mail.headers is object and uid is set (lowercase)', () => {
+      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, {
+        mail: {
+          headers: {
+            'x-uid': 'bar'
+          }
+        }
+      })
+      assert.equal(log.error.callCount, 0)
+      assert.equal(log.warn.callCount, 0)
+    })
+
+    it('logs a warning if message.mail.headers is object and deviceId and uid are missing', () => {
+      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, {
+        mail: {
+          headers: {
+            'X-Template-Name': 'foo',
+            'X-Xxx': 'bar',
+            'X-Yyy': 'baz',
+            'X-Zzz': 'qux'
+          }
+        }
+      }, 'wibble')
+      assert.equal(log.error.callCount, 0)
+      assert.equal(log.warn.callCount, 1)
+      assert.equal(log.warn.args[0].length, 1)
+      assert.deepEqual(log.warn.args[0][0], {
+        op: 'emailHeaders.keys',
+        keys: 'X-Template-Name,X-Xxx,X-Yyy,X-Zzz',
+        template: 'foo',
+        origin: 'wibble'
+      })
+    })
+
+    it('logs a warning if message.headers is object and deviceId and uid are missing', () => {
+      emailHelpers.logErrorIfHeadersAreWeirdOrMissing(log, {
+        headers: {
+          'x-template-name': 'wibble'
+        }
+      }, 'blee')
+      assert.equal(log.error.callCount, 0)
+      assert.equal(log.warn.callCount, 1)
+      assert.deepEqual(log.warn.args[0][0], {
+        op: 'emailHeaders.keys',
+        keys: 'x-template-name',
+        template: 'wibble',
+        origin: 'blee'
+      })
     })
 
     it('logs an error if message.mail.headers is non-object', () => {
@@ -212,6 +290,7 @@ describe('email utils helpers', () => {
         type: 'string',
         origin: 'wibble'
       }, log.error.args[0][0])
+      assert.equal(log.warn.callCount, 0)
     })
 
     it('logs an error if message.headers is non-object', () => {
@@ -222,6 +301,7 @@ describe('email utils helpers', () => {
         type: 'number',
         origin: 'wibble'
       }, log.error.args[0][0])
+      assert.equal(log.warn.callCount, 0)
     })
   })
 })